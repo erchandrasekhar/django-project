475d962de4f7936966753dc0c0c6a84569dbe20a
chandrasekhar yadav <er.chandrasekharyadav@gmail.com>
2019-09-10 23:52:18 +0530

    addition of dynamic destination added

--- travello/views.py
+++ travello/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from .models import Destination
 
 
 def index(request):
-    return render(request, 'index.html')
-
-
+    dest1 = Destination()
+    dest1.name = 'Mumbai'
+    dest1.desc = 'The City That Never Sleep'
+    dest1.price = 700
+    dest2 = Destination()
+    dest2.name = 'Hyderabad'
+    dest2.desc = 'Biryani First than Serewani'
+    dest2.price = 650
+    dest3 = Destination()
+    dest3.name = 'Bangalore'
+    dest3.desc = 'Electronic City'
+    dest3.price = 750
+    return render(request, 'index.html', {'dest1': dest1,'dest2':dest2,'dest3':dest3})
