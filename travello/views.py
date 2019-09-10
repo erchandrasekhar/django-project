f89b3b7f0995b6668f99401170bea86720e34834
chandrasekhar yadav <er.chandrasekharyadav@gmail.com>
2019-09-11 00:22:32 +0530

    daynamic destination displying by for loop

--- travello/views.py
+++ travello/views.py
@@ -6,13 +6,18 @@ def index(request):
     dest1 = Destination()
     dest1.name = 'Mumbai'
     dest1.desc = 'The City That Never Sleep'
+    dest1.img = 'destination_1.jpg'
     dest1.price = 700
     dest2 = Destination()
     dest2.name = 'Hyderabad'
-    dest2.desc = 'Biryani First than Serewani'
+    dest2.desc = 'Biryani First, Than Sherewani'
+    dest2.img = 'destination_2.jpg'
     dest2.price = 650
     dest3 = Destination()
     dest3.name = 'Bangalore'
     dest3.desc = 'Electronic City'
+    dest3.img = 'destination_3.jpg'
     dest3.price = 750
-    return render(request, 'index.html', {'dest1': dest1,'dest2':dest2,'dest3':dest3})
+
+    dests = [dest1,dest2,dest3]
+    return render(request, 'index.html', {'dests':dests})
