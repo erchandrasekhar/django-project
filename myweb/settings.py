0ec14eb58c83d433abd69a5a9fe1e4e4c01c13cf
chandrasekhar yadav <er.chandrasekharyadav@gmail.com>
2019-09-11 20:40:17 +0530

    destination comes from postgres database this stuff are doing

--- myweb/settings.py
+++ myweb/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myweb_python_db',
+        'USER':'postgres',
+        'PASSWORD':'xerox',
+        'HOST':'localhost'
     }
 }
 
@@ -121,3 +125,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
